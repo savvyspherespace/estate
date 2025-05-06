4078bcd4a26e6ae1c94615cc440aafe895053191
Hillary Chukwu <savvyspherespace@gmail.com>
2025-05-06 14:39:22 +0100

    created signup API route

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
 import express from "express";
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
-mongoose.connect(process.env.DATABASE)
+mongoose
+  .connect(process.env.DATABASE)
   .then(() => {
-    console.log('Connected to database');
+    console.log("Connected to database");
   })
   .catch((err) => {
-    console.log('Database connection error:', err);
+    console.log("Database connection error:", err);
   });
 
 const app = express();
+app.use(express.json()),
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-
-app.use("/server/user", userRouter)
+app.use("/server/user", userRouter);
+app.use("/server/auth", authRouter);

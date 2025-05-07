4bb657c8cc55972a1c7f1abdd6360e92786f9ea5
Hillary Chukwu <savvyspherespace@gmail.com>
2025-05-07 16:27:27 +0100

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  server: {
+    proxy: {
+      '/server': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+  plugins: [react(), tailwindcss()],
 })
